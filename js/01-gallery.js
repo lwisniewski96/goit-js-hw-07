dc9caa24726e7f2d7fdb2019808048069389fd3b
lwisniewski96 <113308030+lwisniewski96@users.noreply.github.com>
2023-03-08 22:11:19 +0100

    test

--- js/01-gallery.js
+++ js/01-gallery.js
@@ -1,4 +1,38 @@
-import { galleryItems } from './gallery-items.js';
+// const createGalleryItems = () => {
+//   const items = _.range(0, 14);
+//   .map(() => {
+
+// });
+// console.log(createGalleryItems);
+// }
+
+import { galleryItems } from "./gallery-items.js";
 // Change code below this line
 
+const galleryElements = galleryItems
+  .map(
+    (item) =>
+      `<div class="gallery__item">
+<a class="gallery__link" href="${item.original}">
+<img
+class="gallery__image"
+src="${item.preview}"
+data-source= "${item.original}"
+alt= "${item.description}"
+
+
+
+</a>
+</div>`
+  )
+  .join("");
+
+// const gallery = document.querySelector(".gallery");
+// const imagesHTML = galleryItems.map(({preview, oryginal, descryption})=>{
+// return "<div class="gallery__item"></div>
+// }
+// );
+
+console.log(gallery);
 console.log(galleryItems);
+console.log(newGallery);
