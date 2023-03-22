865460024e73115157265e16b0a87d2228f03aac
lwisniewski96 <113308030+lwisniewski96@users.noreply.github.com>
2023-03-22 22:46:50 +0100

    start to do lightbox 2

--- js/02-lightbox.js
+++ js/02-lightbox.js
@@ -1,4 +1,29 @@
-import { galleryItems } from './gallery-items.js';
+import { galleryItems } from "./gallery-items.js";
 // Change code below this line
+const galleryContainer = document.querySelector(".gallery");
+
+galleryItems.forEach((item) => {
+  const newLi = `
+                <a class="gallery__item" href="${item.original}">
+  <img class="gallery__image" src="${item.preview}" alt= "${item.description}" />
+</a>`;
+
+  galleryContainer.insertAdjacentHTML("beforeend", newLi);
+});
+
+// 2 CZESC ZADANIA i 4 CZESC ZADANIA
+
+galleryContainer.addEventListener("click", (event) => {
+  let originPhoto = event.target.getAttribute("data-source");
+  const instance = basicLightbox.create(`
+  <div class="modal">
+     <img src="${originPhoto}">
+  </div>
+`);
+
+  instance.show();
+
+  console.log(event.target.getAttribute("data-source"));
+});
 
 console.log(galleryItems);
