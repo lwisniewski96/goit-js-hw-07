3a5ac5022ed7af8a9e38dacbef00f7e14fe371d5
lwisniewski96 <113308030+lwisniewski96@users.noreply.github.com>
2023-06-20 00:32:25 +0200

    end

--- js/01-gallery.js
+++ js/01-gallery.js
@@ -1,46 +1,109 @@
 
-
 import { galleryItems } from "./gallery-items.js";
 // Change code below this line
 
-// PIERWSZA CZESC ZADANIA
+const photoPalette = document.querySelector(".gallery");
 
-// Wskazuje na istniejący kontener galerii z html i za pomocą querySelector wywołuje ( przechwycam)
+const markup = galleryItems
+  .map(
+    (img) => `<div class="gallery__item">
+    <a class="gallery__link" href="${img.original}">
+      <img
+        class="gallery__image"
+        src="${img.preview}"
+            data-source="${img.original}"
+             alt="${img.description}"
+      />
+    </a>
+  </div>`
+  )
+  .join("");
 
-const galleryContainer = document.querySelector(".gallery");
+photoPalette.insertAdjacentHTML("afterbegin", markup);
 
-// galleryContainer.append(galleryElements);
+console.log(galleryItems);
 
-galleryItems.forEach((item) => {
-  const newLi = `<div class="gallery__item">
-                  <a class="gallery__link">
-                    <img
-                      class="gallery__image"
-                      src="${item.preview}"
-                      data-source= "${item.original}"
-                      alt= "${item.description}"
-                      >
-                  </a>
-                </div>`;
-  galleryContainer.insertAdjacentHTML("beforeend", newLi);
-});
+photoPalette.addEventListener("click", selectPhoto);
 
-// 2 CZESC ZADANIA i 4 CZESC ZADANIA
+function selectPhoto(event) {
+  if (event.target.nodeName !== "IMG") {
+    return;
+  }
+  event.preventDefault();
 
-galleryContainer.addEventListener("click", (event) => {
-  let originPhoto = event.target.getAttribute("data-source");
-  const instance = basicLightbox.create(`
-  <div class="modal">
-     <img src="${originPhoto}">
-  </div>
-`);
+  const instance = basicLightbox.create(
+    `
+    <img src="${event.target.dataset.source}" width="800" height="600">`
+  );
 
   instance.show();
+}
 
-  console.log(event.target.getAttribute("data-source"));
-});
 
-// 3 CZESC ZADANIA- wgralem w html skrypty
 
-// console.log(galleryContainer);
-// console.log(galleryElements);
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// import { galleryItems } from "./gallery-items.js";
+// // Change code below this line
+
+// // PIERWSZA CZESC ZADANIA
+
+// // Wskazuje na istniejący kontener galerii z html i za pomocą querySelector wywołuje ( przechwycam)
+
+// const galleryContainer = document.querySelector(".gallery");
+
+// // galleryContainer.append(galleryElements);
+
+// galleryItems.forEach((item) => {
+//   const newLi = `<div class="gallery__item">
+//                   <a class="gallery__link">
+//                     <img
+//                       class="gallery__image"
+//                       src="${item.preview}"
+//                       data-source= "${item.original}"
+//                       alt= "${item.description}"
+//                       >
+//                   </a>
+//                 </div>`;
+//   galleryContainer.insertAdjacentHTML("beforeend", newLi);
+// });
+
+// // 2 CZESC ZADANIA i 4 CZESC ZADANIA
+
+// galleryContainer.addEventListener("click", (event) => {
+//   let originPhoto = event.target.getAttribute("data-source");
+//   const instance = basicLightbox.create(`
+//   <div class="modal">
+//      <img src="${originPhoto}">
+//   </div>
+// `);
+
+//   instance.show();
+
+//   console.log(event.target.getAttribute("data-source"));
+// });
+
+// // 3 CZESC ZADANIA- wgralem w html skrypty
+
+// // console.log(galleryContainer);
+// // console.log(galleryElements);
--- js/02-lightbox.js
+++ js/02-lightbox.js
@@ -1,29 +1,64 @@
+// import { galleryItems } from "./gallery-items.js";
+// // Change code below this line
+// const galleryContainer = document.querySelector(".gallery");
+
+// galleryItems.forEach((item) => {
+//   const newLi = `
+//                 <a class="gallery__item" href="${item.original}">
+//   <img class="gallery__image" src="${item.preview}" alt= "${item.description}" />
+// </a>`;
+
+//   galleryContainer.insertAdjacentHTML("beforeend", newLi);
+// });
+
+// // 2 CZESC ZADANIA i 4 CZESC ZADANIA
+
+// galleryContainer.addEventListener("click", (event) => {
+//   let originPhoto = event.target.getAttribute("data-source");
+//   const instance = basicLightbox.create(`
+//   <div class="modal">
+//      <img src="${originPhoto}">
+//   </div>
+// `);
+
+//   instance.show();
+
+//   console.log(event.target.getAttribute("data-source"));
+// });
+
+// console.log(galleryItems);
+
+
+
+
+
+
+
 import { galleryItems } from "./gallery-items.js";
 // Change code below this line
-const galleryContainer = document.querySelector(".gallery");
 
-galleryItems.forEach((item) => {
-  const newLi = `
-                <a class="gallery__item" href="${item.original}">
-  <img class="gallery__image" src="${item.preview}" alt= "${item.description}" />
-</a>`;
+const photoPalette = document.querySelector(".gallery");
 
-  galleryContainer.insertAdjacentHTML("beforeend", newLi);
-});
+const markup = galleryItems
+  .map(
+    (img) => `<div class="gallery__item">
+    <a class="gallery__link" href="${img.original}">
+      <img
+        class="gallery__image"
+        src="${img.preview}"
+            data-source="${img.original}"
+             alt="${img.description}"
+      />
+    </a>
+  </div>`
+  )
+  .join("");
 
-// 2 CZESC ZADANIA i 4 CZESC ZADANIA
-
-galleryContainer.addEventListener("click", (event) => {
-  let originPhoto = event.target.getAttribute("data-source");
-  const instance = basicLightbox.create(`
-  <div class="modal">
-     <img src="${originPhoto}">
-  </div>
-`);
-
-  instance.show();
-
-  console.log(event.target.getAttribute("data-source"));
+photoPalette.insertAdjacentHTML("afterbegin", markup);
+let lightbox = new SimpleLightbox(".gallery a", {
+  captions: true,
+  captionsData: "alt",
+  captionDelay: 250,
 });
 
 console.log(galleryItems);
